0fac13dae11e0d283ddc1a5848c1b92441e589ca
niyaben <niyaben.chaudhari@ontariotechu.net>
2023-11-02 20:44:18 -0400

    a2 second commit

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,16 +4,8 @@
     {
         console.log("App Started");
     }
-    //window.addEventListener("load", Start);
-    //})();
-    document.addEventListener("DOMContentLoaded", function () {
-        // Add an event listener to a button with an id of "myButton"
-        var myButton = document.getElementById("myButton");
-        myButton.addEventListener("click", function () {
-            alert("Button clicked!");
-        });
-    });
-})();
-
+    window.addEventListener("load", Start);
+    })();
+    
 
     

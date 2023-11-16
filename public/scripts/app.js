b718fe3d6fa500471fcb14d57a77bc1bc700eb26
niyaben <niyaben.chaudhari@ontariotechu.net>
2023-11-15 20:10:41 -0500

    third commit

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,6 +3,18 @@
     function Start() 
     {
         console.log("App Started");
+        let deleteButtons = document.querySelectorAll('.btn-danger');
+    for(button of deleteButtons)
+    {
+        button.addEventListener('click',(event)=>{
+            if(!confirm("ARE YOU SURE?"))
+            {
+                event.preventDefault();
+                window.location.assign('/course-list')
+            }
+        })
+    }
+
     }
     window.addEventListener("load", Start);
     })();
--- server/models/course.js
+++ server/models/course.js
@@ -3,7 +3,7 @@ let mongoose = require('mongoose');
 //create a course model
 let Courses = mongoose.Schema({
     course_name: String,
-    course_number: Number,
+    course_number: String,
     Prof_name: String,
     intake: String,
     Description: String,
